b6f3671aa9eeb47e5d446335ad9b5ab7f7d65213
Harisathwik <34621451+Harisathwik@users.noreply.github.com>
2023-05-05 21:16:01 +0530

    Update main.py
    Updated script for SVM

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn import datasets, svm
+import numpy as np
 
+st.title("Support Vector Machine Demo")
 # Load the Iris dataset
-iris_df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',
-                      header=None, names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species'])
+iris = datasets.load_iris()
+iris_df = pd.DataFrame(iris.data, columns=iris.feature_names)
+iris_df['species'] = iris.target
 
 # Sidebar for selecting the range of data
 st.sidebar.header('Select Data Range')
@@ -14,8 +18,27 @@ range_slider = st.sidebar.slider('Select a range of data:', 0, len(iris_df), (0,
 # Filter the dataset based on the selected range
 iris_df_filtered = iris_df.iloc[range_slider[0]:range_slider[1]]
 
-# Plot a scatter plot of sepal length and sepal width with color-coded species
+# Create a Support Vector Machine model
+model = svm.SVC()
+
+# Train the model on the filtered dataset
+X = iris_df_filtered.iloc[:, :-1].values
+y = iris_df_filtered.iloc[:, -1].values
+model.fit(X, y)
+
+# Create a meshgrid to visualize the decision boundary
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.02),
+                     np.arange(y_min, y_max, 0.02))
+
+# Make predictions on the meshgrid
+Z = model.predict(np.c_[xx.ravel(), yy.ravel(), np.zeros_like(xx.ravel()), np.zeros_like(xx.ravel())])
+Z = Z.reshape(xx.shape)
+
+# Plot the decision boundary and the data points
 fig, ax = plt.subplots()
-sns.scatterplot(data=iris_df_filtered, x='sepal_length', y='sepal_width', hue='species', ax=ax)
-ax.set_title('Scatter Plot of Sepal Length vs. Sepal Width')
+sns.scatterplot(data=iris_df_filtered, x='sepal length (cm)', y='sepal width (cm)', hue='species', ax=ax)
+ax.contourf(xx, yy, Z, alpha=0.3)
+ax.set_title('SVM Decision Boundary')
 st.pyplot(fig)
